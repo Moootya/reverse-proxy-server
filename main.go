407b5c88efa6b489e9bc1994470045395c6059d2
Matvey <mat.coniaev2012@yandex.ru>
2020-10-17 11:19:53 +0300

    Прокси-сервер на основе стандартной библиотеки

--- main.go
+++ main.go
@@ -1,35 +1,53 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-
-	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
-func proxy(c *gin.Context) {
-	remote, err := url.Parse("http://localhost:8008")
-	if err != nil {
-		panic(err)
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-
-	proxy.Director = func(req *http.Request) {
-		req.Header = c.Request.Header
-		req.Host = remote.Host
-		req.URL.Scheme = remote.Scheme
-		req.URL.Host = remote.Host
-		req.URL.Path = c.Param("proxyPath")
-	}
-
-	proxy.ServeHTTP(c.Writer, c.Request)
-}
-
 func main() {
-	r := gin.Default()
+	port := 777
+	targetURL := "http://localhost:8008"
 
-	r.Any("/*proxyPath", proxy)
-	r.Run(":8007")
+	u, err := url.Parse(targetURL)
+	if err != nil {
+		log.Fatalf("Could not parse downstream url: %s", targetURL)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(u)
+
+	//proxy.ModifyResponse = func(res *http.Response) error {
+	//	responseContent := map[string]interface{}{}
+	//	err := parseResponse(res, &responseContent)
+	//	if err != nil {
+	//		return err
+	//	}
+	//
+	//	return captureMetrics(responseContent)
+	//}
+
+	director := proxy.Director
+	proxy.Director = func(req *http.Request) {
+		director(req)
+		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+		req.Host = req.URL.Host
+	}
+
+	http.HandleFunc("/", proxy.ServeHTTP)
+	log.Printf("Listening on port %d", port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
 }
+
+//func parseResponse(res *http.Response, unmarshalStruct interface{}) error {
+//	body, err := ioutil.ReadAll(res.Body)
+//	if err != nil {
+//		return err
+//	}
+//	res.Body.Close()
+//
+//	res.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+//	return json.Unmarshal(body, unmarshalStruct)
+//}
