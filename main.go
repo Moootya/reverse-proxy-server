38fdb370bcc100ca2f05d8e00408002baf69a105
Matvey <mat.coniaev2012@yandex.ru>
2020-10-24 14:46:35 +0300

    Добавлена возможность запуска прокси через консоль

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
@@ -8,13 +9,36 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
 	"path/filepath"
 )
 
-func main() {
-	filePath, _ := filepath.Abs("./src.yml")
+var config ServiceConfig
+var TargetUrl, prefixRedirect, name, fileName string
+var listenPort int
 
-	config := getConfig(filePath)
+func init() {
+	flag.StringVar(&fileName, "file", "servers.yml", "File name with settings")
+	flag.StringVar(&TargetUrl, "target", "http://localhost:8008/", "Target url")
+	flag.StringVar(&prefixRedirect, "prefix", "/app/", "Target prefix")
+	flag.StringVar(&name, "name", "Example", "Set name to proxy")
+	flag.IntVar(&listenPort, "listen", 777, "Main listen port")
+}
+
+func main() {
+	flag.Parse()
+	filePath, _ := filepath.Abs(fileName)
+	_, err := os.Stat(filePath)
+
+	if err == nil {
+		config = getConfig(filePath)
+	} else {
+		config.Proxy.ListenPort = listenPort
+		config.Proxy.Servers = []Server{
+			{Name: name, Prefix: prefixRedirect, TargetUrl: TargetUrl},
+		}
+	}
+
 	listenPort := config.Proxy.ListenPort
 	targets := config.Proxy.Servers
 
@@ -37,28 +61,34 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", listenPort), nil))
 }
 
-// Структура для конфига
-type Service struct {
+// Server struct for one server
+type Server struct {
+	Name      string `yaml:"name"`
+	Prefix    string `yaml:"prefix"`
+	TargetUrl string `yaml:"target"`
+}
+
+// ServiceConfig wrapper for Server - structs
+type ServiceConfig struct {
 	Proxy struct {
 		ListenPort int `yaml:"listenPort"`
-		Servers    []struct {
-			Name      string `yaml:"name"`
-			Prefix    string `yaml:"prefix"`
-			TargetUrl string `yaml:"target"`
-		}
+		Servers    []Server
 	}
 }
 
-// getConfig считывает yaml-файл с настройками обратного прокси
-// Возвращает структуру Service
-func getConfig(filePath string) Service {
+// getConfig read and parse Config
+// return ServiceConfig - struct
+func getConfig(filePath string) ServiceConfig {
 
-	var service Service
-	yamlFile, _ := ioutil.ReadFile(filePath)
-
-	err := yaml.Unmarshal(yamlFile, &service)
+	var service ServiceConfig
+	yamlFile, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &service)
+	if err != nil {
+		log.Fatal(err)
 	}
 	return service
 }
